dfcad5d9fcedfa508cab745294b55b694c15fecb
Leo Meng <menglingyang0102@gmail.com>
2023-01-13 01:18:53 -0500

    two pointers

--- Valid Palindrome (ascii version).py
+++ Valid Palindrome (ascii version).py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
 
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l++
+            while r < l and not self.alphaNum(s[r]):
+                r--
+            if s[l].lower != s[r].lower:
+                return False
+            l, r = l + 1, r-1
+
+        return True
+
+    def alphaNum(self, c):
+        return ord('A') <= ord('c') <= ord('Z') or
+        ord('a') <= ord('c') <= ord('z') or
+        ord('0') <= ord('c') <= ord('9')
