2ddbbd2d3edd06a18f19aeee4ca31e67e7f89dd3
Leo Meng <menglingyang0102@gmail.com>
2023-01-13 01:39:12 -0500

    two pointers

--- Valid Palindrome (ascii version).py
+++ Valid Palindrome (ascii version).py
@@ -4,16 +4,14 @@ class Solution(object):
 
         while l < r:
             while l < r and not self.alphaNum(s[l]):
-                l++
-            while r < l and not self.alphaNum(s[r]):
-                r--
-            if s[l].lower != s[r].lower:
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
-            l, r = l + 1, r-1
-
+            l, r = l + 1, r - 1
+        
         return True
-
+    
     def alphaNum(self, c):
-        return ord('A') <= ord('c') <= ord('Z') or
-        ord('a') <= ord('c') <= ord('z') or
-        ord('0') <= ord('c') <= ord('9')
+        return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))
