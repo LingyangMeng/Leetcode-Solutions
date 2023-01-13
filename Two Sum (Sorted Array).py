57e17b74ee6d5264794b5ad15d7e371aad13116e
Leo Meng <menglingyang0102@gmail.com>
2023-01-13 06:32:48 -0500

    T= O(nlogn), S = O(1)

--- Two Sum (Sorted Array).py
+++ Two Sum (Sorted Array).py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        nums.sort()
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            if nums[l] + nums[r] < target:
+                l += 1
+            elif nums[l] + nums[r] > target:
+                r -= 1
+            else:
+                return [nums[l], nums[r]]
+                
+        return
