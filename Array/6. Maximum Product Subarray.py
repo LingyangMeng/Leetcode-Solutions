949f30969ff2c226c8b72a0c6052cad8055d2010
Leo Meng <menglingyang0102@gmail.com>
2023-01-14 04:33:04 -0500

    need two variables curMIn and curMax

--- Array/6. Maximum Product Subarray.py
+++ Array/6. Maximum Product Subarray.py
@@ -1,1 +1,15 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        res = max(nums)
+        curMin, curMax = 1, 1
 
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            tmp = curMax * n
+            curMax = max(curMax * n, curMin * n, n)
+            curMin = max(tmp, curMin * n, n)
+            res = max(curMax, res)
+        
+        return res
